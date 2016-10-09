771542a4da64486033cab82f9718ea52ab3933b8
Joel Hoisko <joel.hoisko@gmail.com>
2016-10-09 19:51:56 +0300

    Very minor addings

--- auctionDjango/models.py
+++ auctionDjango/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    # Maybe tinker with the max_length values?
+    username = models.CharField(max_length=40)
+    password = models.CharField(max_length=128)
+    email = models.CharField(max_length=100)
+
+
+# TODO make proper models
+class Auction(models.Model):
+
+    timestamp = models.DateTimeField('date published')
